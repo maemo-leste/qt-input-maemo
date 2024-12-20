b9ea8ccedd80358e5ec00a04581d5af62b1d39f3
Sander <code@kroket.io>
2024-12-20 17:45:30 +0200

    unbla

--- plugin/event_filter.cpp
+++ plugin/event_filter.cpp
@@ -14,9 +14,7 @@ QHildonEventFilter::QHildonEventFilter(QHildonInputContext* ctx) {
 // Return false to continue normal event processing
 bool QHildonEventFilter::nativeEventFilter(const QByteArray &eventType, void *message, long *result) {
   if (eventType == "xcb_generic_event_t") {
-    auto *event = static_cast<xcb_generic_event_t *>(message);
-    auto bla = event->response_type & ~0x80;
-    switch (bla) {
+    switch (auto *event = static_cast<xcb_generic_event_t *>(message); event->response_type & ~0x80) {
       case XCB_CLIENT_MESSAGE: {
         auto *event_message = reinterpret_cast<xcb_client_message_event_t *>(event);
         if (const xcb_atom_t atom = event_message->type; HILDON_ATOM_MAP.contains(atom)) {
