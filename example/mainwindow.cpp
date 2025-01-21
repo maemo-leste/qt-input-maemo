25f9d0e9a8c02b07325d380b12d1ca530d927b44
Kroket <code@kroket.io>
2025-01-21 22:39:17 +0200

    example: update with a multiline widget

--- example/mainwindow.cpp
+++ example/mainwindow.cpp
@@ -8,7 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
   ui->setupUi(this);
 
   // ui->line->setInputMethodHints(Qt::ImhDigitsOnly);
-  this->appendQuickView();
+  // this->appendQuickView();
 
   this->show();
 }
